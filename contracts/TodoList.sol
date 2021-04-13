84b3d69e60912219cb1939e6ebf40273f9259097
Ariola, Kekai <Kekai.Ariola@adidas.com>
2021-04-12 22:28:53 -0700

    First blockchain project!

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  //state variable - the actual state of the smart contract on the blockchain
+  //similar to class variables
+  uint public taskCount = 0;
+
+  //abi = abstract binary interface
+}
